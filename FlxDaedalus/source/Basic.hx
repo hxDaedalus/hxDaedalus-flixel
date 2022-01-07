19c15aa65e11f98553f1ce8c0e4c3b3385dfa486
nanjizal <none>
2022-01-07 06:51:33 +0000

    add Basic and PathfindingMaze

---
+++ FlxDaedalus/source/Basic.hx
@@ -0,0 +1,69 @@
+package;
+
+import flixel.FlxState;
+
+
+import hxDaedalus.data.ConstraintSegment;
+import hxDaedalus.data.Mesh;
+import hxDaedalus.data.Object;
+import hxDaedalus.data.Vertex;
+import hxDaedalus.factories.RectMesh;
+import hxDaedalus.flx.View;
+
+class Basic extends FlxState {
+    
+    var _mesh:      Mesh;
+    var _view:      View;
+    var _object:    Object;
+    
+    override public function create(){
+        super.create();
+        _view = new View( 0, 0 );
+        add( _view );
+        // build a rectangular 2 polygons mesh of 600x400
+         _mesh = RectMesh.buildRectangle(600, 400);
+         // SINGLE VERTEX INSERTION / DELETION
+         // insert a vertex in mesh at coordinates (550, 50)
+         var vertex : Vertex = _mesh.insertVertex(550, 50);
+        
+         // SINGLE CONSTRAINT SEGMENT INSERTION / DELETION
+         // insert a segment in mesh with end points (70, 300) and (530, 320)
+         var segment : ConstraintSegment = _mesh.insertConstraintSegment(70, 300, 530, 320);
+        
+         // CONSTRAINT SHAPE INSERTION / DELETION
+         // insert a shape in mesh (a crossed square)
+         var shape = _mesh.insertConstraintShape( [
+                          50.,  50., 100.,  50.,      /* 1st segment with end points (50, 50) and (100, 50)       */
+                         100.,  50., 100., 100.,      /* 2nd segment with end points (100, 50) and (100, 100)     */
+                         100., 100.,  50., 100.,      /* 3rd segment with end points (100, 100) and (50, 100)     */
+                          50., 100.,  50.,  50.,      /* 4rd segment with end points (50, 100) and (50, 50)       */
+                          20.,  50., 130., 100.       /* 5rd segment with end points (20, 50) and (130, 100)      */
+                                                 ] );
+                                                
+         // OBJECT INSERTION / TRANSFORMATION / DELETION
+         // insert an object in mesh (a cross)
+         var objectCoords : Array<Float> = new Array<Float>();
+
+         _object = new Object();
+         _object.coordinates = [ -50.,   0.,  50.,  0.,
+                                   0., -50.,   0., 50.,
+                                 -30., -30.,  30., 30.,
+                                  30., -30., -30., 30.
+                                 ];
+         _mesh.insertObject( _object );  // insert after coordinates are setted
+
+         // you can transform objects with x, y, rotation, scaleX, scaleY, pivotX, pivotY
+         _object.x = 400;
+         _object.y = 200;
+         _object.scaleX = 2;
+         _object.scaleY = 2;
+    }   
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+        _view.clear();
+        _object.rotation += 0.05;
+        _mesh.updateObjects();  // don't forget to update
+        _view.drawMesh( _mesh );
+    }
+}
---
+++ FlxDaedalus/source/PathfindingMaze.hx
@@ -0,0 +1,120 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxG;
+import hxDaedalus.GridMaze;
+
+import hxDaedalus.ai.EntityAI;
+import hxDaedalus.ai.PathFinder;
+import hxDaedalus.ai.trajectory.LinearPathSampler;
+import hxDaedalus.data.math.RandGenerator;
+import hxDaedalus.data.Mesh;
+import hxDaedalus.data.Object;
+import hxDaedalus.factories.RectMesh;
+import hxDaedalus.flx.View;
+
+class PathfindingMaze extends FlxState {
+    var _mesh : Mesh;
+    var _view : View;
+    var _entityAI : EntityAI;
+    var _pathfinder : PathFinder;
+    var _path : Array<Float>;
+    var _pathSampler : LinearPathSampler;
+    var _newPath:Bool = false;
+    var x: Float = 0;
+    var y: Float = 0;
+    var rows: Int = 15;
+    var cols: Int = 15;
+    
+    override public function create(){
+        super.create();
+        _view = new View( 0, 0 );
+        add( _view );
+        // build a rectangular 2 polygons mesh of 600x600
+        _mesh = RectMesh.buildRectangle(600, 600);
+        GridMaze.generate(600, 600, cols, rows);
+        _mesh.insertObject(GridMaze.object);
+        _view.constraintsWidth = 4;
+        // we need an entity
+        _entityAI = new EntityAI();
+        // set radius as size for your entity
+        _entityAI.radius = GridMaze.tileWidth * .3;
+        // set a position
+        _entityAI.x = GridMaze.tileWidth / 2;
+        _entityAI.y = GridMaze.tileHeight / 2;
+        // now configure the pathfinder
+        _pathfinder = new PathFinder();
+        _pathfinder.entity = _entityAI;  // set the entity
+        _pathfinder.mesh = _mesh;  // set the mesh
+        // we need a vector to store the path
+        _path = new Array<Float>();
+        // then configure the path sampler
+        _pathSampler = new LinearPathSampler();
+        _pathSampler.entity = _entityAI;
+        _pathSampler.samplingDistance = GridMaze.tileWidth * .7;
+        _pathSampler.path = _path;
+    }
+    function reset(newMaze:Bool = false):Void {
+        var seed = Std.int(Math.random() * 10000 + 1000);
+        if( newMaze ) {
+            _mesh = RectMesh.buildRectangle(600, 600);
+            GridMaze.generate(600, 600, 30, 30, seed);
+            GridMaze.object.scaleX = .92;
+            GridMaze.object.scaleY = .92;
+            GridMaze.object.x = 23;
+            GridMaze.object.y = 23;
+            _mesh.insertObject(GridMaze.object);
+        }
+        _entityAI.radius = GridMaze.tileWidth * .27;
+        _pathSampler.samplingDistance = GridMaze.tileWidth * .7;
+        _pathfinder.mesh = _mesh;
+        _entityAI.x = GridMaze.tileWidth / 2;
+        _entityAI.y = GridMaze.tileHeight / 2;
+        _path = [];
+        _pathSampler.path = _path;
+    }
+    inline function renderDaedalus(){
+        // show result mesh on screen
+        _view.drawMesh( _mesh );
+        if( _newPath ){
+            // find path !
+            _pathfinder.findPath( x, y, _path );
+            // show path on screen
+            _view.drawPath( _path );
+             // show entity position on screen
+            _view.drawEntity( _entityAI ); 
+            // reset the path sampler to manage new generated path
+            _pathSampler.reset();
+        }
+        // animate !
+        if ( _pathSampler.hasNext ) {
+            // move entity
+            _pathSampler.next();
+        }
+        // show entity position on screen
+        _view.drawEntity( _entityAI );
+    }
+    override public function update( elapsed: Float ){
+        super.update(elapsed);
+        if( FlxG.mouse.pressed ){
+            // The left mouse button is currently pressed
+            x = FlxG.mouse.x;
+            y = FlxG.mouse.y;
+        }
+        if( FlxG.mouse.justPressed ){
+            // The left mouse button has just been pressed
+            x = FlxG.mouse.x;
+            y = FlxG.mouse.y;
+            _newPath = true;
+        }
+
+        if( FlxG.mouse.justReleased ){
+            // The left mouse button has just been released
+            x = FlxG.mouse.x;
+            y = FlxG.mouse.y;
+            _newPath = false;
+        }
+        _view.clear();
+        renderDaedalus();
+    }
+}
